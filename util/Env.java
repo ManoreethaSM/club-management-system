4868c442c06e90f39bd5f46106c7f2f81ad57878
ManoreethaSM <147475181+ManoreethaSM@users.noreply.github.com>
2024-07-30 13:55:36 +0530

    Add files via upload

---
+++ util/Env.java
@@ -0,0 +1,7 @@
+package util;
+
+public class Env {
+    public static String databaseURL="jdbc:mysql://localhost:3306/HeyGoodSouls";
+    public static String databaseUser="root";
+    public static String databasePassword="21EC050_mano";
+}
